e4e75b92dbe2b8664aee1d26677e79db6ea954ea
Shubham Singh <singhofficial1611@gmail.com>
2024-11-17 13:52:22 +0530

    Intiaizing Project

---
+++ Frontent/src/App.jsx
@@ -0,0 +1,17 @@
+import Navigation from "./customer/components/Navigation/Navigation.jsx"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <Navigation/>
+    </div>
+     Home
+    </>
+  )
+}
+
+export default App
